d3ee9dc06045cce59e7591166f26b124a1afd59c
ismattullah98 <ismattullah98@gmail.com>
2023-04-09 03:34:13 +0700

    setwallet query

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ let walletAddress = '';
 bot.onText(/\/setsuiwallet/, (msg) => {
   isRecording = true;
   text = msg.text
-  bot.sendMessage(msg.chat.id, 'Silakan masukkan alamat wallet:'+ text);
+  bot.sendMessage(msg.chat.id, msg.chat.id);
 });
 
 bot.on('message', (msg) => {
