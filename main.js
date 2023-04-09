7d3749461ad028a7e8a7291a020673288a32547b
ismattullah98 <ismattullah98@gmail.com>
2023-04-10 00:02:43 +0700

    refix check address

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ let walletAddress = '';
 });*/
 //view wallet Address
 bot.onText(/\/suiwallet/,(msg)=>{
-  const sql = `SELLECT * FROM allusers WHERE telegramid = ?`
+  const sql = `SELECT * FROM allusers WHERE telegramid = ?`
   db.query(sql,msg.chat.id,(err,result)=>{
     if(err){
       console.log(err);
