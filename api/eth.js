ba98a02582c4a659a70ba1c9b0c8e568da6e68a5
ismattullah98 <ismattullah98@gmail.com>
2023-05-22 20:27:28 +0700

    log

--- api/eth.js
+++ api/eth.js
@@ -20,8 +20,7 @@ const subscription = web3.eth.net.isListening().then(()=>{
   const addressesToMonitor = res.map(row=> row.evmwallet)
   web3.eth.subscribe('pendingTransactions').on('data',(txHash)=>{
    web3.eth.getTransaction(txHash, (err,txResult)=>{
-     txValue= parseInt(txResult[1].value) / 10**18
-     console.log(txValue)
+     
      if(!err && txResult && addressesToMonitor.includes(txResult.to.toLowerCase())){
        const weiValue = txResult.value;
        const etherValue = parseInt(weiValue) / 10**18;
