8b1c62e2f6e47f9506a76cfa57520bb4d67280f4
ismatullah98 <ismatullah98@gmail.com>
2023-05-23 21:57:24 +0700

    MultiSubscribe

--- api/module/subevm.js
+++ api/module/subevm.js
@@ -16,22 +16,24 @@ let subEvm = {
             
             if(res.length>1){
                 res.map(row => {
+                let nameWallet = `<a href="${providerName.explorer}adress/${row.evmwallet}">${row.namewallet? row.namewallet:tx.to.slice(0,4)+'..'+tx.to.slice(-2)}</a>`
                 let hash = `<a href="${providerName.explorer}tx/${tx.hash}">txHash</a>`
                 let TrueValue= web3.utils.fromWei(tx.value, 'ether');
                 const formattedValue = parseFloat(TrueValue) >= 0.0001 ? parseFloat(TrueValue).toFixed(6).replace(/\.?0*$/, '') : parseFloat(TrueValue);
                 let from = '<a href="'+providerName.explorer+'adress/'+tx.from+'">'+tx.from.slice(0,4)+'..'+tx.from.slice(-2)+'</a>'
-                message+= `Chain: ${providerName.name} \n 
-                You Has Received ${formattedValue} ${providerName.symbol} estimated() ${hash} from ${from} `
+                
+                message+= `Chain: ${providerName.name} \n ${nameWallet} You Has Received ${formattedValue} ${providerName.symbol} from ${from} ${hash} `
                     bot.sendMessage(row.telegramid,message,{parse_mode: 'HTML'})
                   });
             }
             if(res.length == 1){
+                let nameWallet = `<a href="${providerName.explorer}adress/${row.evmwallet}">${row.namewallet? row.namewallet:tx.to.slice(0,4)+'..'+tx.to.slice(-2)}</a>`
                 let hash = `<a href="${providerName.explorer}tx/${tx.hash}">txHash</a>`
                 let TrueValue= web3.utils.fromWei(tx.value, 'ether');
                 const formattedValue = parseFloat(TrueValue) >= 0.0001 ? parseFloat(TrueValue).toFixed(6).replace(/\.?0*$/, '') : parseFloat(TrueValue);
                 let from = '<a href="'+providerName.explorer+'adress/'+tx.from+'">'+tx.from.slice(0,4)+'..'+tx.from.slice(-2)+'</a>'
-                message+= `Chain: ${providerName.name} \n 
-                You Has Received ${formattedValue} ${providerName.symbol} estimated() ${hash} from ${from} `
+                
+                message+= `Chain: ${providerName.name} \n ${nameWallet} You Has Received ${formattedValue} ${providerName.symbol} from ${from} ${hash}`
                 bot.sendMessage(res[0].telegramid, message,{parse_mode: 'HTML'});
             }
         })
